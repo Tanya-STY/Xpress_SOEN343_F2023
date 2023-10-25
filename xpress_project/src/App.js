f38094d3b23275196920e3addb1a3aee29862ce5
Tanya So Tin Yan <tanya.sty99@gmail.com>
2023-10-25 00:32:19 -0400

    Create NavBar (base version)

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import './App.css';
+// import Navbar from './components/Navbar/Navbar';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     // <Router>
+//     //    <Navbar/>
+//     // <Router/>
+//     <Router>
+//       <Navbar/>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar'; // Import your Navbar component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+    </Router>
   );
 }
 
