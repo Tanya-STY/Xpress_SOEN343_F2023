f32ffd0d95a3a3d7d44a784bd1b21bae142f04ce
Tanya So Tin Yan <tanya.sty99@gmail.com>
2023-11-03 00:42:33 -0400

    UserDetails and PaymentDetails pages created

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'; // Import your Navbar component
+import UserDetails from './Pages/UserDetails/UserDetails';
 // import Navbar_SignedIn from './components/Navbar_SignedIn/Navbar_SignedIn'
 
 function App() {
   return (
     <Router> 
-      <Navbar/>
+      {/* <Navbar/> */}
+      <UserDetails/>
     </Router>
   );
 }
