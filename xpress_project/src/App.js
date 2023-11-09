a9f744d943fee92663742b1f9ca6d43ce6d3b4f1
Tanya So Tin Yan <tanya.sty99@gmail.com>
2023-11-09 11:26:18 -0500

    Testing front-end of the user-details package

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -21,7 +21,7 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <UserDetail />
     </div>
   );
