eda631c4fde6d8c43b66594a1373068352a20c72
shamma13 <shammamarkis@hotmail.com>
2023-11-09 23:39:26 -0500

    Quotation page implementation

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -1,6 +1,6 @@
 
 // import React from 'react';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 // import './App.css';
 // import Navbar from './components/Navbar/Navbar'; // Import your Navbar component
 
@@ -17,12 +17,26 @@
 
 import UserDetail from './Pages/UserDetail/UserDetail'; // Adjust the import path as needed
 import Navbar from './components/Navbar/Navbar';
+import Quotation from './components/Quotation/Quotation';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <UserDetail />
+      {/* <Navbar/>
+      <UserDetail /> */}
+      {/* <Quotation/> */}
+
+      <Router>
+      <Routes>
+        <Route path="/" element={<Quotation/>}/>
+        {/* <Route path="/x" element={<x/>}/>
+        <Route path="/y" element={<y/>}/>
+        <Route path="/z" element={<z/>}/>
+        <Route path="/*" element={<NotFound/>}/> */}
+      </Routes>
+    </Router>
+
+
     </div>
   );
 }
--- xpress_project/src/components/Currency/Currency.js
+++ xpress_project/src/components/Currency/Currency.js
@@ -9,4 +9,3 @@ export const Currency = () => {
   );
 };
 
-export default Currency;
