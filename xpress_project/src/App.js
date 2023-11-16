955b0ebe144675d62b245daa93fb28a564471507
Ihana Fahmy <ihanafahmy@gmail.com>
2023-11-16 18:39:44 -0500

    On branch ihana Your branch is up to date with 'origin/ihana'.
    Changes to be committed:
    	modified:   xpress_project/src/App.js

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -1,19 +1,3 @@
-// import './App.css';
-// import Navbar from './components/Navbar/Navbar';
-// import { BrowserRouter as Router } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     // <Router>
-//     //    <Navbar/>
-//     // <Router/>
-//     <Router>
-//       <Navbar/>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import React from 'react';
 // import { BrowserRouter as Router } from 'react-router-dom';
