e46b986b3b25bca052b9a10850098e622c5e2831
dogmen6 <doghmanefadoua@gmail.com>
2023-11-27 14:10:11 -0500

    Addition of the Update Status feature

--- xpress_project/src/components/StatusBar/StatusBar.js
+++ xpress_project/src/components/StatusBar/StatusBar.js
@@ -1,57 +1,59 @@
-//// StatusBar.js
 //
+//// StatusBar.js
 //import React, { useState } from 'react';
 //import './StatusBar.css';
 //
-//const StatusBar = () => {
+//const StatusBar = ({ onRequestStatusChange }) => {
 //  const steps = ['Started', 'Processed', 'Picked Up', 'In Transit', 'Delivered'];
 //  const [currentStep, setCurrentStep] = useState(1);
 //
 //  const handleNext = () => {
 //    if (currentStep < steps.length) {
 //      setCurrentStep((prevStep) => prevStep + 1);
+//      onRequestStatusChange(steps[currentStep]);
 //    }
 //  };
 //
 //  return (
-//    <div className="step-progress-bar">
-//      <div className="steps">
-//        {steps.map((step, index) => (
-//          <div
-//            key={index}
-//            className={`step ${currentStep > index ? 'completed' : ''} ${currentStep === index + 1 ? 'active' : ''}`}
-//          >
-//            {index + 1}
-//          </div>
-//        ))}
+//      <div className="step-progress-bar">
+//        <div className="steps">
+//          {steps.map((step, index) => (
+//            <div
+//              key={index}
+//              className={`step ${currentStep > index ? 'completed' : ''} ${currentStep === index + 1 ? 'active' : ''}`}
+//            >
+//              {index + 1}
+//            </div>
+//          ))}
+//        </div>
+//        <div className="step-labels">
+//          {steps.map((step, index) => (
+//            <div key={index} className={`step-label ${currentStep > index ? 'completed' : ''}`}>
+//              {step}
+//            </div>
+//          ))}
+//        </div>
+//        <div className="connector-line">
+//          {Array.from({ length: steps.length - 1 }, (_, index) => (
+//            <div
+//              key={index}
+//              className={`connector ${currentStep > index ? 'completed' : ''} ${
+//                currentStep === index + 1 ? 'active' : ''
+//              }`}
+//            ></div>
+//          ))}
+//        </div>
+//        <button className="next-button" onClick={handleNext}>
+//          Next
+//        </button>
 //      </div>
-//      <div className="step-labels">
-//        {steps.map((step, index) => (
-//          <div key={index} className={`step-label ${currentStep > index ? 'completed' : ''}`}>
-//            {step}
-//          </div>
-//        ))}
-//      </div>
-//      <div className="connector-line">
-//        {Array.from({ length: steps.length - 1 }, (_, index) => (
-//          <div
-//            key={index}
-//            className={`connector ${currentStep > index ? 'completed' : ''} ${
-//              currentStep === index + 1 ? 'active' : ''
-//            }`}
-//          ></div>
-//        ))}
-//      </div>
-//      <button className="next-button" onClick={handleNext}>
-//        Next
-//      </button>
-//    </div>
-//  );
-//};
+//    );
+//  };
 //
 //export default StatusBar;
+//
+////check handle events
 
-// StatusBar.js
 import React, { useState } from 'react';
 import './StatusBar.css';
 
@@ -61,45 +63,43 @@ const StatusBar = ({ onRequestStatusChange }) => {
 
   const handleNext = () => {
     if (currentStep < steps.length) {
-      setCurrentStep((prevStep) => prevStep + 1);
       onRequestStatusChange(steps[currentStep]);
+      setCurrentStep((prevStep) => prevStep + 1);
     }
   };
 
   return (
-      <div className="step-progress-bar">
-        <div className="steps">
-          {steps.map((step, index) => (
-            <div
-              key={index}
-              className={`step ${currentStep > index ? 'completed' : ''} ${currentStep === index + 1 ? 'active' : ''}`}
-            >
-              {index + 1}
-            </div>
-          ))}
-        </div>
-        <div className="step-labels">
-          {steps.map((step, index) => (
-            <div key={index} className={`step-label ${currentStep > index ? 'completed' : ''}`}>
-              {step}
-            </div>
-          ))}
-        </div>
-        <div className="connector-line">
-          {Array.from({ length: steps.length - 1 }, (_, index) => (
-            <div
-              key={index}
-              className={`connector ${currentStep > index ? 'completed' : ''} ${
-                currentStep === index + 1 ? 'active' : ''
-              }`}
-            ></div>
-          ))}
-        </div>
-        <button className="next-button" onClick={handleNext}>
-          Next
-        </button>
+    <div className="step-progress-bar">
+      <div className="steps">
+        {steps.map((step, index) => (
+          <div
+            key={index}
+            className={`step ${currentStep > index ? 'completed' : ''} ${currentStep === index + 1 ? 'active' : ''}`}
+          >
+            {index + 1}
+          </div>
+        ))}
       </div>
-    );
-  };
+      <div className="step-labels">
+        {steps.map((step, index) => (
+          <div key={index} className={`step-label ${currentStep > index ? 'completed' : ''}`}>
+            {step}
+          </div>
+        ))}
+      </div>
+      <div className="connector-line">
+        {Array.from({ length: steps.length - 1 }, (_, index) => (
+          <div
+            key={index}
+            className={`connector ${currentStep > index ? 'completed' : ''} ${currentStep === index + 1 ? 'active' : ''}`}
+          ></div>
+        ))}
+      </div>
+      <button className="next-button" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default StatusBar;
