221e6f339fb3db8261d54c2f47e49458f92a5536
mzmddi <110358757+mzmddi@users.noreply.github.com>
2023-11-28 18:51:08 -0500

    recomit same vers of app.js than main branch

--- xpress_project/src/App.js
+++ xpress_project/src/App.js
@@ -1,26 +1,29 @@
 
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar/Navbar'; // Import your Navbar component
-import Navbar_SignedUp from './components/Navbar_SignedIn/Navbar_SignedIn';
-import UserDetails from './Pages/UserDetails/UserDetails';
-// import Navbar_SignedIn from './components/Navbar_SignedIn/Navbar_SignedIn'
-import RequestPage from './components/RequestPage/RequestPage'
-import {useJsApiLoader, GoogleMap } from '@react-google-maps/api'
+// import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import './App.css';
+// import Navbar from './components/Navbar/Navbar'; // Import your Navbar component
+
+
+// function App() {
+//   return (
+//     <Router> 
+//       <Navbar/>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import UserDetail from './Pages/UserDetail/UserDetail'; // Adjust the import path as needed
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-
-  
-
   return (
-    <Router> 
+    <div className="App">
       {/* <Navbar/> */}
-      {/* <UserDetails/> */}
-      {/*<Navbar_SignedUp/>*/}
-      <RequestPage/>
-
-    </Router>
+      <UserDetail />
+    </div>
   );
 }
 
