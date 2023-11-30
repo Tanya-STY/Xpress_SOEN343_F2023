65d3ebe265ea3ca1bd520b6f1482355bd6886b9e
dogmen6 <doghmanefadoua@gmail.com>
2023-11-29 21:44:12 -0500

    Addition of delivery estimate date feature

--- xpress_project/src/components/User/DeliveryRequestStatus.js
+++ xpress_project/src/components/User/DeliveryRequestStatus.js
@@ -1,13 +1,69 @@
-
+//
+//
+//// DeliveryRequestStatus.js
+//import React, { useState, useEffect } from 'react';
+//import { useParams, Link } from 'react-router-dom';
+//import StatusBar from '../StatusBar/StatusBar.js';
+//import './DeliveryRequestStatus.css';
+//
+//const DeliveryRequestStatus = () => {
+//  const { requestNumber } = useParams();
+//  const [status, setStatus] = useState('Started');
+//  const [estimatedDeliveryDate, setEstimatedDeliveryDate] = useState('');
+//
+//  useEffect(() => {
+//    const storedStatus = localStorage.getItem(`status_${requestNumber}`);
+//    if (storedStatus) {
+//      setStatus(storedStatus);
+//    }
+//
+//    const deliveryDate = localStorage.getItem(`date_${requestNumber}`) || '2023-01-01';
+//
+//    const deliveryDateObject = new Date(deliveryDate);
+//    const estimatedDateObject = new Date(deliveryDateObject);
+//    estimatedDateObject.setDate(deliveryDateObject.getDate() + 7);
+//
+//    const formattedEstimatedDate = estimatedDateObject.toISOString().split('T')[0];
+//
+//    setEstimatedDeliveryDate(formattedEstimatedDate);
+//  }, [requestNumber]);
+//
+//  const renderReviewButton = () => {
+//    if (status === 'Delivered') {
+//      return (
+//        <Link to={`/review/${requestNumber}`} className="review-button">
+//          Review
+//        </Link>
+//      );
+//    }
+//    return null;
+//  };
+//
+//  return (
+//    <div className="delivery-request-status-container">
+//      <h1 className="page-title">Delivery Request Status</h1>
+//      <div className="status-bar-wrapper">
+//        <StatusBar currentStatus={status} showNextButton={false} />
+//      </div>
+//      <p>Estimated Delivery Date: {estimatedDeliveryDate}</p>
+//      {renderReviewButton()}
+//      <Link to="/">Back to Homepage</Link>
+//    </div>
+//  );
+//};
+//
+//export default DeliveryRequestStatus;
 
 // DeliveryRequestStatus.js
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom';
 import StatusBar from '../StatusBar/StatusBar.js';
 import './DeliveryRequestStatus.css';
 
 const DeliveryRequestStatus = () => {
   const { requestNumber } = useParams();
+  const { search } = useLocation();
+  const creationDate = new URLSearchParams(search).get('creationDate') || '2023-01-01';
   const [status, setStatus] = useState('Started');
   const [estimatedDeliveryDate, setEstimatedDeliveryDate] = useState('');
 
@@ -17,16 +73,15 @@ const DeliveryRequestStatus = () => {
       setStatus(storedStatus);
     }
 
-    const deliveryDate = localStorage.getItem(`date_${requestNumber}`) || '2023-01-01';
-
-    const deliveryDateObject = new Date(deliveryDate);
+    // Calculate the estimated delivery date (7 days after the initial order date)
+    const deliveryDateObject = new Date(creationDate);
     const estimatedDateObject = new Date(deliveryDateObject);
     estimatedDateObject.setDate(deliveryDateObject.getDate() + 7);
 
     const formattedEstimatedDate = estimatedDateObject.toISOString().split('T')[0];
 
     setEstimatedDeliveryDate(formattedEstimatedDate);
-  }, [requestNumber]);
+  }, [requestNumber, creationDate]);
 
   const renderReviewButton = () => {
     if (status === 'Delivered') {
@@ -44,12 +99,13 @@ const DeliveryRequestStatus = () => {
       <h1 className="page-title">Delivery Request Status</h1>
       <div className="status-bar-wrapper">
         <StatusBar currentStatus={status} showNextButton={false} />
+        {renderReviewButton()}
       </div>
       <p>Estimated Delivery Date: {estimatedDeliveryDate}</p>
-      {renderReviewButton()}
       <Link to="/">Back to Homepage</Link>
     </div>
   );
 };
 
 export default DeliveryRequestStatus;
+
